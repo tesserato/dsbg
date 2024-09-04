e7d43def2c21697c1034ad6bb5b94d65b8d25c9d
Carlos <tesserato@hotmail.com>
2024-09-04 14:19:12 +0100

    improved

--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"fmt"
-	// "io/fs"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
+	// "regexp"
 	"strings"
 	"time"
 
-	// "github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/gocolly/colly/v2"
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
 	"gopkg.in/yaml.v2"
+)
+
+const (
+	defaultTitle      = "My Blog"
+	defaultContentDir = "content"
+	defaultOutputDir  = "public"
+	defaultIndexName  = "index.html"
+	defaultDateFormat = "2006-01-02"
 )
 
 type Article struct {
@@ -22,37 +31,46 @@ type Article struct {
 	Updated     time.Time `yaml:"updated"`
 	Tags        []string  `yaml:"tags"`
 	Content     string    `yaml:"-"`
+	Files       []string  `yaml:"-"`
+	IsPage      bool      `yaml:"-"`
+	Path        string    `yaml:"-"`
 }
 
 func main() {
-	// 1. Read all markdown files from the content folder
-	files, err := filepath.Glob("content/*.md")
+	// 1. Read all markdown and HTML files from the content folder
+	files, err := getFiles(defaultContentDir, []string{".md", ".html"})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 2. Parse each markdown file into an Article struct
+	// 2. Parse each file into an Article struct
 	var articles []Article
 	for _, file := range files {
-		article, err := parseArticle(file)
+		article, err := parseFile(file)
 		if err != nil {
-			log.Println("Error parsing article:", err)
+			log.Printf("Error parsing file %s: %s\n", file, err)
 			continue
 		}
 		articles = append(articles, article)
 	}
 
-	// 3. Generate HTML for each article
+	// 3. Generate HTML for each article and page
 	for _, article := range articles {
-		err := generateArticleHTML(article)
-		if err != nil {
-			log.Println("Error generating article HTML:", err)
-			continue
+		if article.IsPage {
+			err := generatePageHTML(article, defaultOutputDir)
+			if err != nil {
+				log.Printf("Error generating HTML for page %s: %s\n", article.Title, err)
+			}
+		} else {
+			err := generateArticleHTML(article, defaultOutputDir)
+			if err != nil {
+				log.Printf("Error generating HTML for article %s: %s\n", article.Title, err)
+			}
 		}
 	}
 
 	// 4. Generate the index.html file
-	err = generateIndexHTML(articles)
+	err = generateIndexHTML(articles, defaultOutputDir, defaultIndexName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -60,13 +78,32 @@ func main() {
 	log.Println("Blog generated successfully!")
 }
 
-func parseArticle(file string) (Article, error) {
+func getFiles(root string, extensions []string) ([]string, error) {
+	var files []string
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			for _, ext := range extensions {
+				if strings.HasSuffix(path, ext) {
+					files = append(files, path)
+					break
+				}
+			}
+		}
+		return nil
+	})
+	return files, err
+}
+
+func parseFile(file string) (Article, error) {
 	data, err := os.ReadFile(file)
 	if err != nil {
 		return Article{}, err
 	}
 
-	// Split the file content into frontmatter and markdown
+	// Split the file content into frontmatter and content
 	frontmatter, content := splitFrontmatterAndContent(string(data))
 
 	// Parse the frontmatter
@@ -76,9 +113,6 @@ func parseArticle(file string) (Article, error) {
 		return Article{}, err
 	}
 
-	// Convert markdown to HTML
-	article.Content = string(blackfriday.Run([]byte(content)))
-
 	// Set default values for created and updated if not provided
 	if article.Created.IsZero() {
 		article.Created = time.Now()
@@ -86,6 +120,22 @@ func parseArticle(file string) (Article, error) {
 	if article.Updated.IsZero() {
 		article.Updated = article.Created
 	}
+
+	// Convert markdown to HTML
+	if strings.HasSuffix(file, ".md") {
+		article.Content = string(blackfriday.Run([]byte(content)))
+	} else {
+		article.Content = content
+	}
+
+	// Extract resources from HTML
+	article.Files = extractResources(article.Content)
+
+	// Determine if the article is a page
+	article.IsPage = contains(article.Tags, "PAGE")
+
+	// Set the article path
+	article.Path = filepath.Dir(file)
 
 	return article, nil
 }
@@ -98,9 +148,31 @@ func splitFrontmatterAndContent(data string) (string, string) {
 	return parts[1], parts[2]
 }
 
-func generateArticleHTML(article Article) error {
+func extractResources(html string) []string {
+	var resources []string
+	c := colly.NewCollector()
+	c.OnHTML("img[src], script[src], link[href]", func(e *colly.HTMLElement) {
+		resources = append(resources, e.Attr("src"))
+	})
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Error extracting resources:", err)
+	})
+	c.Visit("http://example.com/" + html)
+	return resources
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func generateArticleHTML(article Article, outputDir string) error {
 	// Create the article folder if it doesn't exist
-	articleDir := fmt.Sprintf("public/%s", strings.ReplaceAll(article.Title, " ", "-"))
+	articleDir := filepath.Join(outputDir, strings.ReplaceAll(article.Title, " ", "-"))
 	err := os.MkdirAll(articleDir, 0755)
 	if err != nil {
 		return err
@@ -114,22 +186,25 @@ func generateArticleHTML(article Article) error {
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>%s</title>
+		<link rel="stylesheet" href="/style.css">
 	</head>
 	<body>
-		<h1>%s</h1>
-		<p>Created: %s</p>
-		<p>Updated: %s</p>
-		<ul>
+		<article>
+			<h1>%s</h1>
+			<p>Created: %s</p>
+			<p>Updated: %s</p>
+			<ul>
+				%s
+			</ul>
 			%s
-		</ul>
-		%s
+		</article>
 	</body>
 	</html>
 	`,
 		article.Title,
 		article.Title,
-		article.Created.Format("2006-01-02"),
-		article.Updated.Format("2006-01-02"),
+		article.Created.Format(defaultDateFormat),
+		article.Updated.Format(defaultDateFormat),
 		generateTagsHTML(article.Tags),
 		article.Content,
 	)
@@ -139,7 +214,47 @@ func generateArticleHTML(article Article) error {
 	html = p.Sanitize(html)
 
 	// Write the HTML content to the file
-	filename := fmt.Sprintf("%s/index.html", articleDir)
+	filename := filepath.Join(articleDir, defaultIndexName)
+	return os.WriteFile(filename, []byte(html), 0644)
+}
+
+func generatePageHTML(article Article, outputDir string) error {
+	// Create the page folder if it doesn't exist
+	pageDir := filepath.Join(outputDir, article.Path)
+	err := os.MkdirAll(pageDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	// Generate the HTML content
+	html := fmt.Sprintf(`
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>%s</title>
+		<link rel="stylesheet" href="/style.css">
+	</head>
+	<body>
+		<article>
+			<h1>%s</h1>
+			%s
+		</article>
+	</body>
+	</html>
+	`,
+		article.Title,
+		article.Title,
+		article.Content,
+	)
+
+	// Sanitize the HTML content
+	p := bluemonday.UGCPolicy()
+	html = p.Sanitize(html)
+
+	// Write the HTML content to the file
+	filename := filepath.Join(pageDir, defaultIndexName)
 	return os.WriteFile(filename, []byte(html), 0644)
 }
 
@@ -151,20 +266,31 @@ func generateTagsHTML(tags []string) string {
 	return html
 }
 
-func generateIndexHTML(articles []Article) error {
+func generateIndexHTML(articles []Article, outputDir, filename string) error {
 	// Generate the article list HTML
 	var articleList string
 	for _, article := range articles {
-		articleList += fmt.Sprintf(`
-		<li>
-			<a href="%s">%s</a>
-			<p>%s</p>
-		</li>
-		`,
-			fmt.Sprintf("/%s/", strings.ReplaceAll(article.Title, " ", "-")),
-			article.Title,
-			article.Description,
-		)
+		if !article.IsPage {
+			articleLink := strings.ReplaceAll(article.Title, " ", "-") + "/"
+			articleList += fmt.Sprintf(`
+			<li>
+				<a href="%s">%s</a>
+				<p>%s</p>
+				<p>Created: %s</p>
+				<p>Updated: %s</p>
+				<ul>
+					%s
+				</ul>
+			</li>
+			`,
+				articleLink,
+				article.Title,
+				article.Description,
+				article.Created.Format(defaultDateFormat),
+				article.Updated.Format(defaultDateFormat),
+				generateTagsHTML(article.Tags),
+			)
+		}
 	}
 
 	// Generate the index.html content
@@ -174,19 +300,23 @@ func generateIndexHTML(articles []Article) error {
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>My Blog</title>
+		<title>%s</title>
+		<link rel="stylesheet" href="/style.css">
 	</head>
 	<body>
-		<h1>My Blog</h1>
+		<h1>%s</h1>
 		<ul>
 			%s
 		</ul>
 	</body>
 	</html>
 	`,
+		defaultTitle,
+		defaultTitle,
 		articleList,
 	)
 
 	// Write the HTML content to the file
-	return os.WriteFile("public/index.html", []byte(html), 0644)
+	filePath := filepath.Join(outputDir, filename)
+	return os.WriteFile(filePath, []byte(html), 0644)
 }
