1fc310893b2c6c2c5f68af6c3aa82cbcc4e4db93
Carlos <tesserato@hotmail.com>
2025-01-03 02:45:45 +0000

    implemented go releaser action
    https://goreleaser.com/ci/actions/

--- gen_icon.ps1
+++
@@ -1,3 +0,0 @@
-magick -density 376 -background none "logo.svg" "content/logo.webp"
-
-magick -background none "content/logo.webp" -fill red -opaque black -blur 0x1  -crop 167x167+0+0  "assets/favicon.ico"
