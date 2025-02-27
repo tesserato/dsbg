be61aed40ed7c75dbbd9b912b746035274f59615
Carlos <tesserato@hotmail.com>
2025-02-27 18:49:13 +0000

    added gen_codebase.ps1 back to source control

---
+++ gen_codebase.ps1
@@ -0,0 +1,4 @@
+# Script to generate codebase.md from the codebase using CodeWeaver
+# https://github.com/tesserato/codeweaver
+
+codeweaver -ignore="public\\.+,\.git.*,.+\.exe,.+\.[Pp][Nn][Gg],.+\.[Jj][Pp][Ee]?[Gg],.+\.[Mm][Pp][34],.+\.[Aa][Vv][Ii],.+\.[Mm][Oo][Vv],.+\.[Ww][Mm][Aa],.+\.[Pp][Dd][Ff],.+\.[Dd][Oo][Cc][Xx]?,.+\.[Xx][Ll][Ss][Xx]?,.+\.[Pp][Pp][Tt][Xx]?,.+\.[Zz][Ii][Pp],.+\.[Rr][Aa][Rr],.+\.[7][Zz],.+\.[Ii][Ss][Oo],.+\.[Bb][Ii][Nn],.+\.[Dd][Aa][Tt],.+\.[Dd][Mm][Gg],.+\.[Gg][Ii][Ff],.+\.[Tt][Ii][Ff][Ff]?,.+\.[Pp][Ss][Dd],.+\.[Mm][Kk][Vv],.+\.[Ff][Ll][Aa][Cc],.+\.[Oo][Gg][Gg],.+\.[Ww][Ee][Bb][Pp],.+\.[Aa][Vv][Ii][Ff],.+\.[Hh][Ee][Ii][Cc],.+\.[Jj][Xx][Ll],codebase.md,DRAFT.md,.+\.[Ii][Cc][Oo],.+\.svg"
